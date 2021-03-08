ca11d0f54c09a5ae437c32062e1b2f95fe690430
WalidSafi <32139490+WalidSafi@users.noreply.github.com>
2021-03-07 23:52:00 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -13,8 +13,10 @@ import matplotlib.pyplot as plt
 
 def differentialEvolution(population):
 
+    # Loop For each Vector in the population to apply Mutation Crossover and Offspring Selection
     for index in range(0,100):
 
+        #Initiate variables
         a = 0
         b = 0
         c = 0
@@ -22,11 +24,13 @@ def differentialEvolution(population):
         distribution = [1 - Cr, Cr]
         target_vector = population[index]
 
+        #Loop untill a,b,c and index are unique
         while a == b or a == c or b == c or a == index or b == index or c == index:
             a = random.randint(0,99)
             b = random.randint(0,99)
             c = random.randint(0,99)
 
+        #initialize vectors with indexes at a,b,c
         vec_a = np.array(population[a])
         vec_b = np.array(population[b])
         vec_c = np.array(population[c])
@@ -36,18 +40,22 @@ def differentialEvolution(population):
         #print(vec_b)
         #print(vec_c)
 
+        #First step of mutation (Difference of two parent vectors)
         vector_scale = np.subtract(vec_c,vec_b)
         #print(vector_scale)
 
-        vector_scale = vector_scale * 0.8
+        #scale difference using Scaling Factor defined
+        vector_scale = vector_scale * F
 
+        #add the trail vector (Vector_scale) with 3rd parent vector
         vector_scale = np.add(vec_a, vector_scale)
 
+        #loop at every index of the vector
         for j in range(0,5):
-            cross = random.choices(p,distribution)
-            if(cross[0] == 1):
+            cross = random.choices(p,distribution) # Determines probability of crossover occuring
+            if(cross[0] == 1): #Crossover complete, offspring uses trail vector value at index j
                 offspring[j] = vector_scale[j]
-            else:
+            else: #No crossover, uses target vectors value at index j
                 offspring[j] = target_vector[j]
 
 
@@ -60,25 +68,25 @@ def differentialEvolution(population):
         #print("Target Vector: " + str(x))
         #print("Offpsring Vector: " + str(y))
 
-        if (fitness(offspring) <= fitness(target_vector)):
+        if (fitness(offspring) <= fitness(target_vector)): #Checks the fitness of the target vector against the offspring
             #print("Offspring is better")
-            population[index] = offspring
+            population[index] = offspring #population updated with newer and fitter offspring
         else:
             #print("Target Vector is better")
-            population[index] = target_vector
+            population[index] = target_vector #Population not updated as offspring was more unfit
 
     #print(population)
     return population
 
 def fitness(individual):
 
-    #x**2 + y**2 +
+    # Random function of x1^2 + x2^2 +x3^2 +x4^2 +x5^2 for testing
     score = (individual[0]**2) + (individual[1]**2) + (individual[2]**2) + (individual[3]**2) + (individual[4]**2)
     #print(score)
     return score
 
-# Press the green button in the gutter to run the script.
-def initial():
+
+def initial(): #generate initial population
 
     genpop = [0]*100
     variables = [0]*5
